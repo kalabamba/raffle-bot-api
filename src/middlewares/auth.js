bf5d26f52dbf3999a80a93fd89aa8e17d8b4e96c
Turgut Memiş <kalabamba49@gmail.com>
2023-08-08 18:50:24 +0300

    fix for auth middleware due change login header

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 module.exports = function (req, res, next) {
-	const token = req.header('x-auth-token');
+	const token = req.header('Authorization').split(' ')[1];
 	if (!token) {
 		return res.status(401).send({error: "Access denied. No token provided."});
 	}
