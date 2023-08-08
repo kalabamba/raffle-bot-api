a82324bc306a9e36d0a45347a5c1e015764fcee9
Turgut Memiş <kalabamba49@gmail.com>
2023-08-08 19:18:43 +0300

    fix to no token provide

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 module.exports = function (req, res, next) {
-	const token = req.header('Authorization').split(' ')[1];
-	if (!token) {
+	const rawToken = req.header('Authorization');
+	if (!rawToken) {
 		return res.status(401).send({error: "Access denied. No token provided."});
 	}
 	try {
+		const token = rawToken.split(' ')[1];
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 		req.user = decoded;
 		next();
