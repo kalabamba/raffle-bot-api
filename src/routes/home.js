68d3dd388b47e69643e198b3547221c06d3ab9e8
Turgut Memiş <kalabamba49@gmail.com>
2023-08-09 12:55:46 +0300

    add auth to home route

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.status(403).send({error:'Access denied.'});
+router.get('/',[auth], (req, res) => {
+	res.status(200).send({});
 	});
 module.exports = router;
