39eb4cf02963f2717382bcab085532039a8df30e
Turgut Memiş <kalabamba49@gmail.com>
2023-08-09 13:01:09 +0300

    fix import path

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const auth = require('../middlewares/auth');
 
 router.get('/',[auth], (req, res) => {
 	res.status(200).send({});
