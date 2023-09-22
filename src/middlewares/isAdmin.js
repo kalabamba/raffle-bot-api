304c35349366543c435fd352ff83718708510676
Turgut Memis <kalabamba49@gmail.com>
2023-09-23 00:35:59 +0300

    Update isAdmin.js

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,4 +1,4 @@
 module.exports = function (req, res, next) {
-	if (!req?.user.isAdmin) return res.status(403).send({error: "Access denied."});
+	if (!req?.user?.isAdmin) return res.status(403).send({error: "Access denied."});
 	next();
 }
